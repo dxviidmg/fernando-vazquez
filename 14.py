6579b1829901395a5399c839120abf4acc061f22
David <david@ixulabs.com>
2021-09-10 15:05:01 -0500

    4 y 14

---
+++ 14.py
@@ -0,0 +1,22 @@
+
+#n_viajes = int(input('Numero de viajes'))
+
+r = 1
+
+personas_list = []
+pesos_list = []
+while r == 1:
+    personas = int(input('Cantidad de personas: '))
+    peso = int(input('Peso del viaje: '))
+
+    personas_list.append(personas)
+    pesos_list.append(peso)
+    r = int(input('Si desea agregar otro viaje, oprima 1: '))
+
+
+print()
+print('Cantidad de viajes', len(personas_list))
+print('Cantidad de personas trasportadas', sum(personas_list))
+print('Peso transportado', sum(pesos_list))
+print('Promedio de personas por viaje', sum(personas_list)/len(personas_list))
+print('Promedio de peso por viaje', sum(pesos_list)/len(pesos_list))
---
+++ 4.py
@@ -0,0 +1,28 @@
+def fibonacci(n):
+    if n == 0 or n == 1:
+        return n
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+
+def suma_fibonacci():
+    n = 0
+    r = 0
+    suma = 0
+
+    while True:
+        r = fibonacci(n)
+        n+=1
+        if r < 100:
+            continue
+
+        if r > 10000:
+            break
+
+        suma += r
+
+    print(suma)
+
+def main():
+    suma_fibonacci()
+
+main()
